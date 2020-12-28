3bfd483dac318439b30d016953fc53cdf3e2be76
Maciej Rzemek <>
2020-12-28 15:24:47 +0100

    Added network module

--- app/src/main/java/com/example/akademiaandroida/core/DI/KoinInjector.kt
+++ app/src/main/java/com/example/akademiaandroida/core/DI/KoinInjector.kt
@@ -1,2 +1,7 @@
 package com.example.akademiaandroida.core.DI
 
+import org.koin.core.module.Module
+
+var koinInjector: List<Module> = listOf(
+    networkModule
+)
--- app/src/main/java/com/example/akademiaandroida/core/app/App.kt
+++ app/src/main/java/com/example/akademiaandroida/core/app/App.kt
@@ -1,4 +1,23 @@
 package com.example.akademiaandroida.core.app
 
-class App {
+import android.app.Application
+import com.example.akademiaandroida.core.DI.koinInjector
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
+
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+    }
+
+
+    private fun startKoin() {
+        startKoin {
+            androidContext(this@App)
+            modules(koinInjector)
+        }
+    }
 }
+
